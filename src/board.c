e8fd2afd7afde57e31abf9c4d24f11c5465f23b7
rayil <rayil@uci.edu>
2021-05-08 21:01:57 -0700

    Implemented AI movement

--- src/board.c
+++ src/board.c
@@ -17,6 +17,7 @@
 #include "board.h"
 #include "legalityCheck.h"
 #include "movelist.h"
+#include "ai.h"
 
 // game runner that loops through human vs human chess game
 void playerVsPlayer(char gameBoard[8][8][2]) 
@@ -66,13 +67,17 @@ void playerVsAI(char gameBoard[8][8][2]) {
 			// append the move list here
 			append(list,gameBoard);
 			// here is where AI makes its move
+			makeMove(gameBoard,'b');
+			printBoard(gameBoard);
 			append(list,gameBoard);
 		} else if (playerColor == 'b') {
 			// here is where AI makes its move
+			makeMove(gameBoard,'w');
 			if (winCheck(gameBoard)) {
 				break;
 			}
 			append(list,gameBoard);
+			printBoard(gameBoard);
 			playerInput(gameBoard, 'b');
 			printBoard(gameBoard);
 			// append the move list here
